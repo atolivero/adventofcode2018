fe8e78a0d4a12efc90f781a901f8982bd1de07a4
Alexander Olivero <alexander.t.olivero@gmail.com>
2018-12-01 12:20:58 -0500

    Refactor to not use sum as variable name
    Do this to not overwrite standard names

--- solutions/problem1.py
+++ solutions/problem1.py
@@ -16,17 +16,18 @@ PATH_REFS = Path(__file__).parents[1] / 'references'
 
 def part_one() -> int:
     """Calculate the solution to part one"""
-    sum = 0
+    total = 0
     problem_one_ref = PATH_REFS / 'problem1.txt'
     with problem_one_ref.open('r') as infile:
         for line in infile.readlines():
             if line[0] == '+':
-                sum += int(line[1:])
+                total += int(line[1:])
             else:
-                sum -= int(line[1:])
-    return sum
+                total -= int(line[1:])
+    return total
 
 def part_two() -> int:
+    """Calculate the solution to part two"""
     current_value = 0
     frequencies = []
     problem_one_ref = PATH_REFS / 'problem1.txt'
