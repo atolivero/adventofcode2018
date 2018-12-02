6edf48ff31ebc5a1dff8dd7f5ce09da38d451ed6
Alexander Olivero <alexander.t.olivero@gmail.com>
2018-12-02 05:44:39 -0500

    Add template for solution
    Do this to follow standard coding style

---
+++ solutions/problem2.py
@@ -0,0 +1,27 @@
+"""
+### Code Owner: Alexander Olivero
+
+### Objective:
+    House the logic to solve problem 2
+
+### Developer Notes:
+    Data found here: https://adventofcode.com/2018/day/2/input
+"""
+from pathlib import Path
+
+PATH_REFS = Path(__file__).parents[1] / 'references'
+
+#-----------------------------------------------------------#
+#-----------------------------------------------------------#
+
+
+
+def main() -> int:
+    """House the logic for this problem"""
+
+
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
