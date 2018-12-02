06354070c3671f86cf01cd2d800cc1117c97dec2
Alexander Olivero <alexander.t.olivero@gmail.com>
2018-12-02 06:11:27 -0500

    Add solution to part one of problem
    Do this to solve things

--- solutions/problem2.py
+++ solutions/problem2.py
@@ -15,10 +15,29 @@ PATH_REFS = Path(__file__).parents[1] / 'references'
 #-----------------------------------------------------------#
 
 
+def check_letter_count(id: str, occurances: int) -> bool:
+    """Takes a int argument and returns true if any letter occurs that many times or more in the string argument"""
+    for letter in list(set(id)):
+        if id.count(letter) == occurances:
+            return True
+
+def part_one() -> int:
+    """Calculate the solution for part one"""
+    two_count = 0
+    three_count = 0
+    problem_one_ref = PATH_REFS / 'problem2.txt'
+    with problem_one_ref.open('r') as infile:
+        for line in infile.readlines():
+            if check_letter_count(line.strip(), 2):
+                two_count += 1
+            if check_letter_count(line.strip(), 3):
+                three_count += 1
+    return two_count * three_count
+
 
 def main() -> int:
     """House the logic for this problem"""
-
+    print('Solution to part one is: {}'.format(part_one()))
 
     return 0
 
