2704c3f4c316cf8b5798829838c22cadc2f97f43
Alexander Olivero <alexander.t.olivero@gmail.com>
2018-12-09 06:28:32 -0500

    Add fixes for problem 5 script
    Do this so it can run successfully

--- solutions/problem5.py
+++ solutions/problem5.py
@@ -8,7 +8,6 @@
     Data found here: https://adventofcode.com/2018/day/5/input
 """
 from pathlib import Path
-from difflib import ndiff
 
 PATH_REFS = Path(__file__).parents[1] / 'references'
 
@@ -28,7 +27,7 @@ def part_one(line: str) -> int:
     return len(line)
 
 
-def part_two(line: str) -> int:
+def part_two(line: str) -> str:
     """Calculate the solution for part two"""
     alphabet = []
     for letter in range(97, 123):
@@ -44,10 +43,10 @@ def part_two(line: str) -> int:
 def main() -> int:
     """House the logic for this problem"""
     problem_five_ref = PATH_REFS / 'problem5.txt'
-    infile = problem_five_ref.open('r'):
-    line = infile.readline().strip():
+    infile = problem_five_ref.open('r')
+    line = infile.readline().strip()
     
-    print('Solution to part one is: {}'.format(part_one(line))
+    print('Solution to part one is: {}'.format(part_one(line)))
     print('Solution to part two is: {}'.format(part_two(line)))
 
     return 0
